c32d298a028b2c60d6ea21023fbc66b1fd349fcd
Jeffrey Raj <jeffreyrajcj@gmail.com>
2023-05-08 13:31:30 +0530

    Added functionality to range selector to select number of cells in grid

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ const selectedColor = document.querySelector("#colorPicker");
 const slider= document.querySelector("#slider");
 const resetButton = document.querySelector("#resetButton");
 const eraserButton = document.querySelector("#eraserButton");
-let gridSize = slider.value;
-let cellWidth = Math.floor(750/gridSize,0)+'px';
-let cellHeight = Math.floor(750/gridSize,0)+'px';
+const gridSizeDisplay =  document.querySelector("#gridSizeDisplay");
+let gridSize = slider.value * slider.value;
+let cellWidth;
+let cellHeight;
+newCellSize(gridSize);
 createGrid(gridSize);
 console.log(selectedColor.value);
+SizeDisplay(gridSize);
 
+//create cells to be added to container
 function createCells(){
     const cell = document.createElement('div');
     cell.classList.add('cell');
@@ -16,7 +20,25 @@ function createCells(){
     cell.style.width = cellHeight;
     return cell;
 }
+//display size of grid
+function SizeDisplay(gridSize){
+    gridSizeDisplay.textContent=` ${gridSize} x ${gridSize}`;
 
+}
+//remove all child nodes in the grid container
+function removeAllChildDivs(parent){
+    while(parent.firstChild)
+    {
+        parent.removeChild(parent.firstChild);
+    }
+
+}
+//calculate cell size based on range selected
+function newCellSize(gridSize){
+     cellWidth = Math.floor(750/gridSize,0)+'px';
+     cellHeight = Math.floor(750/gridSize,0)+'px';
+}
+//create grid by appending cells
 function createGrid(gridSize){
     for(i=0;i< gridSize; i++){
         for(j=0;j<gridSize;j++)
@@ -26,19 +48,31 @@ function createGrid(gridSize){
         }
         
     }
+
+//change color of cells on mouse over
 sketchPadContainer.addEventListener('mouseover',(e)=>{
     if(e.target.matches('.cell'))
         {
             e.target.style.backgroundColor = selectedColor.value;
         }
 })
-    
+// reload page on page reset
 resetButton.addEventListener("click",()=>{
     window.location.reload();
 })
-
+ // eraser button
 eraserButton.addEventListener("click",()=>{
     selectedColor.value='#FFFFFF';
+})
+
+//change number of cells in grid when range input changes
+slider.addEventListener("input",(e)=>{
+    gridSize = slider.value * slider.value;
+    SizeDisplay(gridSize);//display new grid size
+    removeAllChildDivs(sketchPadContainer); // remove existing cells
+    newCellSize(gridSize);
+    createGrid(gridSize);
+
 })
 
 }
